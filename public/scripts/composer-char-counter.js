b83c5bc28aa39d58fcd2522666c81429cf90160c
Abdelrahman Zaytoun <abdelrahmanzaytounn@gmail.com>
2021-10-09 19:48:10 -0400

    adding counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+
+
+
+        $(document).ready(function () {
+            $(".new-tweet").on("keydown keyup", "textarea", function () {
+          
+              // create stored variable to determine # chars left
+              let $charInTweet = $(this).val();
+              let $charRemain = 140 - $charInTweet.length;
+          
+              // traverse up and down the DOM to get character count
+              let $counter = $(this).closest("form").find(".counter");
+              $counter.text($charRemain);
+          
+              // make counter red when tweet > 140 chars
+              if ($charRemain < 0) {
+                $counter.addClass("overtweeted");
+              } else {
+                $counter.removeClass("overtweeted");
+              }
+            });
+          });
+          
