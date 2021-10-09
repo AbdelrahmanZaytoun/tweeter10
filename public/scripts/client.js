ba63551d1c9aa8fa6c17f29ad9fa91880c4da60e
Abdelrahman Zaytoun <abdelrahmanzaytounn@gmail.com>
2021-10-09 19:34:23 -0400

    creating createTweetElement which will return tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,3 +28,42 @@ const tweetData = [
       "created_at": 14611139588
     }
   ];
+
+
+
+
+const createTweetElement = function(tweet) {
+    let $date = Math.floor((Date.now() - tweet.created_at) / 86400000);
+    let $tweet = (
+      `<article class="tweet">
+          <header>
+            <div class="tweet-author">
+              <div class="user-pic">
+                <img src="${tweet.user.avatars}">
+              </div>
+              <div class="name">
+                ${tweet.user.name}
+              </div>
+            </div>
+            <div class="handle">${tweet.user.handle}
+            </div>
+          </header>
+  
+          <div class="tweet-text">
+          ${escape(tweet.content.text)}
+          </div>
+  
+          <footer>
+            <div>${$date} days ago</div>
+  
+            <div class="tweet-icons">
+            <i class="far fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="far fa-heart"></i>
+            </div>
+  
+          </footer>
+  
+        </article>`);
+    return $tweet;
+  };
