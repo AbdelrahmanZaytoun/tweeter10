3c6a1b91284233eeb11c12df0e8b659173e37df6
Abdelrahman Zaytoun <abdelrahmanzaytounn@gmail.com>
2021-10-09 19:32:23 -0400

    assign a tweetData variable into client.js file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,27 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Abdo",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@ABZAY"
+      },
+      "content": {
+        "text": "Hello Tweeter!"
+      },
+      "created_at": 14611139588
+    }
+  ];
